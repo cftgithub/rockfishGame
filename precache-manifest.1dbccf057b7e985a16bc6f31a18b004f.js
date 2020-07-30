9ea468bbab24ce84250c66eb981e0b7599c77da0
cftgithub <chiafongt@gmail.com>
2020-07-30 17:59:51 -0600

    Updates

---
+++ precache-manifest.1dbccf057b7e985a16bc6f31a18b004f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa5778f64e03a6aa8fa140d57f6b5005",
+    "url": "/rockfishGame/index.html"
+  },
+  {
+    "revision": "bd9412b6e910f1ddff71",
+    "url": "/rockfishGame/static/css/main.0f477a98.chunk.css"
+  },
+  {
+    "revision": "e55ed680437e28c06a1d",
+    "url": "/rockfishGame/static/js/2.66198967.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rockfishGame/static/js/2.66198967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd9412b6e910f1ddff71",
+    "url": "/rockfishGame/static/js/main.34aa287f.chunk.js"
+  },
+  {
+    "revision": "23902c6bfe4b520be4d3",
+    "url": "/rockfishGame/static/js/runtime-main.ffacef3b.js"
+  }
+]);
