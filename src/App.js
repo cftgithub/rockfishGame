d46ffa05c2a747ea3b50e415064abffb7b05fea3
cftgithub <chiafongt@gmail.com>
2020-07-31 18:33:59 -0600

    added remove and sort function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { Component } from "react";
 import Title from "./components/Title";
 import FishCard from "./components/FishCard";
 import Wrapper from "./components/Wrapper";
 import fish from "./fish.json";
 import './App.css';
 
-class App extends React.Component {
+class App extends Component {
   state = {
     fish,
     currCount: 0,
@@ -14,34 +14,35 @@ class App extends React.Component {
     instruction: "There are 6 fishes on the 'Good' list. Can you catch them all?"
   };
 
-  increaseScore = () => {
-    if(this.state.currCount < 15) {
-      this.setState({ currCount: this.state.currCount + 1 })
-    } else {
-      this.endGame();
-    }
-  };
+  // increaseScore = () => {
+  //   if(this.state.currCount < 15) {
+  //     this.setState({ currCount: this.state.currCount + 1 })
+  //   } else {
+  //     this.endGame();
+  //   }
+  // };
 
-  resetScore = () => {
-    if(this.state.currCount > this.state.hiCount) {
-      this.setState({ hiCount: this.state.currCount })
-    };
-    this.setState({ currCount: 0 });
-  };
+  // resetScore = () => {
+  //   if(this.state.currCount > this.state.hiCount) {
+  //     this.setState({ hiCount: this.state.currCount })
+  //   };
+  //   this.setState({ currCount: 0 });
+  // };
 
-  randomizeCards = () => {
-    this.state.fish.sort(() => Math.random() - 0.5);
-  }
+  // randomizeCards = () => {
+  //   this.state.fish.sort(() => Math.random() - 0.5);
+  // }
 
-  endGame = () => {
-    this.setState({ currCount: this.state.currCount + 1 })
-    this.setState({ title: "Fish Catcher!" });
-    this.setState({ hiCount: 16 });
-  }
+  // endGame = () => {
+  //   this.setState({ currCount: this.state.currCount + 1 })
+  //   this.setState({ title: "Fish Catcher!" });
+  //   this.setState({ hiCount: 16 });
+  // }
 
   // clicker = id => {
   //   if(this.state.currCount !== 16) {
   //   const selected = this.state.fish.filter(fsh => fsh.id === id);
+  //   console.log(selected)
   //   if(selected[0].clicked === false) {
   //     this.increaseScore();
   //     selected[0].clicked = true;
@@ -61,6 +62,13 @@ class App extends React.Component {
   //   this.randomizeCards();
   // };
 
+  removeFish = id => {
+    this.state.fish.sort(() => Math.random() - 0.5);
+    const fish = this.state.fish.filter(fish => fish.id !== id);
+    console.log (fish);
+    this.setState({ fish });
+  };
+  
   render() {
     return (
       <Wrapper>
@@ -69,21 +77,21 @@ class App extends React.Component {
           instruction={this.state.instruction}
           currentScore={"Current Score: " + this.state.currCount}
           highScore={"High Score: " + this.state.hiCount}
-        />
-        {this.state.fish.map((fish, index) => {
-          return <FishCard 
-          key={index}
-          id={fish.id}
-          species={fish.commonName}
-          scientific={fish.sciName}
-          image={fish.image}
-          status={fish.status}
-          clicked={fish.clicked}
-          handleOnClick={this.clicker}
-          />})}
+          />
+        {this.state.fish.map(fish => (
+          <FishCard
+            key={fish.id}
+            id={fish.id}
+            species={fish.commonName}
+            scientific={fish.sciName}
+            image={fish.image}
+            status={fish.status}
+            removeFish={this.removeFish}
+          />
+        ))}
       </Wrapper>
     );
-  };
-};
+  }
+}
 
 export default App;
--- src/components/FishCard/index.js
+++ src/components/FishCard/index.js
@@ -1,23 +1,44 @@
 import React from "react";
 import "./style.css";
 
-class FishCard extends React.Component {
+// class FishCard extends React.Component {
 
-clicker = () => {
-    this.props.handleOnClick(this.props.id)
-};
+// clicker = () => {
+//     this.props.handleOnClick(this.props.id)
+// };
 
-render() {
+// render() {
+//     return (
+//         <div className="card" onClick={this.clicker}>
+//             <div className="img-container">
+//                 <img alt={this.props.species} src={this.props.image}/>
+//             </div>
+//             <div>{this.props.species}</div>
+//             <div>{this.props.status}</div>
+//         </div>
+//     );
+// }
+// }
+
+function FishCard(props) {
     return (
-        <div className="card" onClick={this.clicker}>
-            <div className="img-container">
-                <img alt={this.props.species} src={this.props.image}/>
-            </div>
-            <div>{this.props.species}</div>
-            <div>{this.props.status}</div>
+      <div className="card">
+        <div className="img-container">
+          <img alt={props.species} src={props.image} />
         </div>
+        <div className="content">
+          <ul>
+            <li>
+              <strong>Name:</strong> {props.species}
+            </li>
+            <li>
+              <strong>Status:</strong> {props.status}
+            </li>
+          </ul>
+        </div>
+        <span onClick={() => props.removeFish(props.id)} className="remove">𝘅</span>
+      </div>
     );
-}
-}
+  }
 
 export default FishCard;
